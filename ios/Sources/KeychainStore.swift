ec5d713b089f5ae0600b014368841d92751d814c
mikelaurenzo7-collab <mikelaurenzo7@gmail.com>
2026-06-13 10:32:23 -0500

    ios: add Keychain token storage

---
+++ ios/Sources/KeychainStore.swift
@@ -0,0 +1,54 @@
+import Foundation
+import Security
+
+struct KeychainStore {
+    let service: String
+
+    func save(_ value: String, account: String) throws {
+        let data = Data(value.utf8)
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account
+        ]
+        let attributes: [String: Any] = [kSecValueData as String: data]
+        let status = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
+        if status == errSecItemNotFound {
+            var addQuery = query
+            addQuery[kSecValueData as String] = data
+            let addStatus = SecItemAdd(addQuery as CFDictionary, nil)
+            guard addStatus == errSecSuccess else { throw KeychainError.unhandledStatus(addStatus) }
+            return
+        }
+        guard status == errSecSuccess else { throw KeychainError.unhandledStatus(status) }
+    }
+
+    func read(account: String) throws -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+        if status == errSecItemNotFound { return nil }
+        guard status == errSecSuccess, let data = result as? Data else { throw KeychainError.unhandledStatus(status) }
+        return String(data: data, encoding: .utf8)
+    }
+
+    func remove(account: String) throws {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account
+        ]
+        let status = SecItemDelete(query as CFDictionary)
+        guard status == errSecSuccess || status == errSecItemNotFound else { throw KeychainError.unhandledStatus(status) }
+    }
+}
+
+enum KeychainError: Error {
+    case unhandledStatus(OSStatus)
+}
