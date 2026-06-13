90d05edc21ff969c31d9111388933de62c04a9aa
mikelaurenzo7-collab <mikelaurenzo7@gmail.com>
2026-06-13 11:26:28 -0500

    ios: fix CarPlay scene inheritance compile blocker

--- ios/Sources/CarScene.swift
+++ ios/Sources/CarScene.swift
@@ -1,7 +1,7 @@
 import CarPlay
 import UIKit
 
-final class MeAICarScene: UIResponder, CPTemplateApplicationSceneDelegate {
+class MeAICarScene: UIResponder, CPTemplateApplicationSceneDelegate {
     func templateApplicationScene(_ scene: CPTemplateApplicationScene, didConnect controller: CPInterfaceController) {
         let item = CPListItem(text: "Me.AI", detailText: "Ready")
         let section = CPListSection(items: [item])
